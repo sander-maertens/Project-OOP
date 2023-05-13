ea62503ef4e5fe33dfeaf2903507bbe06904b1bd
DESKTOP-3CG0F67\sander <sandermaertens2301@gmail.com>
2023-05-13 15:35:54 +0200

    leadeboard

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,121 +1,199 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 using System.Windows.Threading;
+using System.IO;
+using System.Windows.Controls.Primitives;
+
 
 namespace SimonSays
 {
-    public partial class MainWindow
+    public partial class MainWindow : Window
     {
-        private readonly List<Color> _sequence = new List<Color>();
-        private int _currentStep = 0;
-        private readonly Random _random = new Random();
-        private readonly DispatcherTimer _timer = new DispatcherTimer();
-        private bool _inputEnabled = false;
+        private List<Button> simonSaysButtons;
+        private List<Button> playerButtons;
+        private List<int> simonSaysSequence;
+        private int currentSequenceIndex;
+        private Random random;
+        private DispatcherTimer timer;
+        private int score;
 
         public MainWindow()
         {
             InitializeComponent();
-            _timer.Interval = TimeSpan.FromSeconds(1);
-            _timer.Tick += Timer_Tick;
-            NewGame();
+
+            // Initialize lists of buttons and the random number generator
+            simonSaysButtons = new List<Button> { RedButton, BlueButton, GreenButton, YellowButton };
+            playerButtons = new List<Button>();
+            simonSaysSequence = new List<int>();
+            random = new Random();
+
+            // Set up button event handlers
+            RedButton.Click += Button_Click;
+            BlueButton.Click += Button_Click;
+            GreenButton.Click += Button_Click;
+            YellowButton.Click += Button_Click;
+            StartButton.Click += StartButton_Click;
+            ResetButton.Click += ResetButton_Click;
+
+            // Set up timer for Simon Says sequence playback
+            timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 1);
         }
 
-        private void InitializeComponent()
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
-        }
+            // Add the button that was clicked to the player's list
+            Button button = sender as Button;
+            playerButtons.Add(button);
 
-        private void NewGame()
-        {
-            _sequence.Clear();
-            _currentStep = 0;
-            AddStepToSequence();
-            _inputEnabled = false;
-            _timer.Start();
-        }
-
-        public void AddStepToSequence()
-        {
-            var colors = new[] { Colors.Red, Colors.Blue, Colors.Green, Colors.Yellow };
-            _sequence.Add(colors[_random.Next(colors.Length)]);
-        }
-
-        private void ShowSequence()
-        {
-            _inputEnabled = false;
-            _currentStep = 0;
-            var i = 0;
-            _timer.Tick += (sender, args) =>
+            // Check if the player's sequence matches the current Simon Says sequence
+            bool isCorrect = true;
+            for (int i = 0; i < playerButtons.Count; i++)
             {
-                if (i < _sequence.Count)
+                if (playerButtons[i] != simonSaysButtons[simonSaysSequence[i]])
                 {
-                    LightUpButton(_sequence[i]);
-                    i++;
-                }
-                else
-                {
-                    _timer.Stop();
-                    _inputEnabled = true;
-                }
-            };
-            _timer.Start();
-        }
-
-        private void CheckInput(Color color)
-        {
-            if (!_inputEnabled) return;
-            LightUpButton(color);
-            if (_sequence[_currentStep] == color)
-            {
-                _currentStep++;
-                if (_currentStep == _sequence.Count)
-                {
-                    AddStepToSequence();
-                    ShowSequence();
+                    isCorrect = false;
+                    break;
                 }
             }
-            else
+
+            // If the player's sequence was incorrect, end the game
+            if (!isCorrect)
             {
-                MessageBox.Show("Game over!");
-                NewGame();
+                MessageBox.Show($"Game over! Your score is {score}");
+                ResetGame();
+            }
+
+            // If the player's sequence matches the entire Simon Says sequence, add another button to the sequence and replay it
+            if (playerButtons.Count == simonSaysSequence.Count)
+            {
+                score += 10;
+                AddToSimonSaysSequence();
+                currentSequenceIndex = 0;
+                timer.Start();
+                playerButtons.Clear();
             }
         }
 
-        public void LightUpButton(Color color)
+
+
+        private void AddToSimonSaysSequence()
         {
-            var button = color switch
-            {
-                Colors.Red => RedButton,
-                Colors.Blue => BlueButton,
-                Colors.Green => GreenButton,
-                Colors.Yellow => YellowButton,
-                _ => throw new ArgumentOutOfRangeException(nameof(color), color, null)
-            };
-            var originalColor = button.Background;
-            button.Background = new SolidColorBrush(color);
-            _timer.Tick += (sender, args) => button.Background = originalColor;
-            _timer.Start();
+            // Add a random button to the Simon Says sequence
+            int buttonIndex = random.Next(0, 4);
+            simonSaysSequence.Add(buttonIndex);
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            _timer.Stop();
-            if (!_inputEnabled)
+            // Play back the current button in the Simon Says sequence and advance to the next button
+            simonSaysButtons[simonSaysSequence[currentSequenceIndex]].Opacity = 1;
+            currentSequenceIndex++;
+            if (currentSequenceIndex >= simonSaysSequence.Count)
             {
-                LightUpButton(Colors.Black);
-                ShowSequence();
+                timer.Stop();
+            }
+            else
+            {
+                simonSaysButtons[simonSaysSequence[currentSequenceIndex - 1]].Opacity = 0.5;
             }
         }
 
-        private void RedButton_Click(object sender, RoutedEventArgs e) => CheckInput(Colors.Red);
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Start a new game of Simon Says
+            ResetGame();
+            AddToSimonSaysSequence();
+            currentSequenceIndex = 0;
+            timer.Start();
+        }
 
-        private void BlueButton_Click(object sender, RoutedEventArgs e) => CheckInput(Colors.Blue);
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Reset the game of Simon Says
+            ResetGame();
+        }
+        private void ResetGame()
+        {
+            // Save the player's score to the leaderboard if it is greater than 0
+            if (score > 0)
+            {
+                SaveScoreToLeaderboard(score);
+            }
 
-        private void GreenButton_Click(object sender, RoutedEventArgs e) => CheckInput(Colors.Green);
+            // Reset the game of Simon Says
+            simonSaysSequence.Clear();
+            playerButtons.Clear();
+            timer.Stop();
+            foreach (Button button in simonSaysButtons)
+            {
+                button.Opacity = 0.5;
+            }
+            score = 0;
+        }
+        private void SaveScoreToLeaderboard(int score)
+        {
+            // Save the player's score to a text file on the desktop
+            string leaderboardPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\SimonSaysLeaderboard.txt";
+            List<int> scores = new List<int>();
 
-        private void YellowButton_Click(object sender, RoutedEventArgs e) => CheckInput(Colors.Yellow);
+            if (File.Exists(leaderboardPath) && new FileInfo(leaderboardPath).Length > 0)
+            {
+                scores = File.ReadAllLines(leaderboardPath).Select(int.Parse).ToList();
+            }
+
+            scores.Add(score);
+            scores.Sort((a, b) => b.CompareTo(a));
+            if (scores.Count > 10)
+            {
+                scores.RemoveAt(10);
+            }
+            File.WriteAllLines(leaderboardPath, scores.Select(x => x.ToString()).ToArray());
+        }
+
+
+        private void AddToLeaderboard(int score)
+        {
+            // Save the player's score to a text file on the desktop
+            string leaderboardPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\SimonSaysLeaderboard.txt";
+            List<int> scores = new List<int>();
+
+            if (File.Exists(leaderboardPath) && new FileInfo(leaderboardPath).Length > 0)
+            {
+                scores = File.ReadAllLines(leaderboardPath).Select(int.Parse).ToList();
+            }
+
+            scores.Add(score);
+            scores.Sort((a, b) => b.CompareTo(a));
+            if (scores.Count > 10)
+            {
+                scores.RemoveAt(10);
+            }
+            File.WriteAllLines(leaderboardPath, scores.Select(x => x.ToString()).ToArray());
+        }
+
+
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Set the opacity of all Simon Says buttons to 0.5
+            foreach (Button button in simonSaysButtons)
+            {
+                button.Opacity = 0.5;
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // Clean up resources when the window is closing
+            timer.Stop();
+            timer.Tick -= Timer_Tick;
+        }
+
     }
 }
